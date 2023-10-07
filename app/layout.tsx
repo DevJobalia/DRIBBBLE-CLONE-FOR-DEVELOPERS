36d3c272f83b2d9ceee9349654842baa99e7cbe6
Dev Jobalia <webdev.jobalia@gmail.com>
2023-10-07 23:28:40 +0530

    Add: NavBar and Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
+
 export const metadata = {
   title: "Flexibble",
   description: "Showcase and discover remarkable developer projects",
@@ -12,7 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        Navbar <main>{children}</main>Footer
+        <Navbar /> <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
